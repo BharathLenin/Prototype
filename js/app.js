ca628ba13cb6d17d816f5787e20b161c5710146b
BXK8340 <Bharath_Kasinathan1@homedepot.com>
2016-06-26 00:38:21 -0400

    Added chart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
 angular.module('myApp', [
+	'c3-angularjs',
     'myApp.controllers'
 ])
--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,16 +1,29 @@
 angular.module('myApp.controllers', [])
     .controller('homeController', function($scope, $rootScope) {
 
-    	$scope.showItem = 'Main';
+        $scope.showItem = 'Main';
 
-        // $scope.setSelected = function(item) {
-        //     console.log(item);
-        //     console.log($scope.showItem);
-        // };
 
-        $scope.openComponent= function(item) {
-        	alert(item);
-        	$scope.showItem = item;
+        $scope.localChart = {
+            columns: [
+                ['green', 32],
+                ['amber', 20],
+                ['red', 10]
+            ],
+            type: 'donut',
+            colors: {
+                green: '#009900',
+                amber: '#FFC200',
+                red: '#ff0000'
+            },
+            labels: true,
+            bindto: "#chart1"
+        };
+
+        $scope.openComponent = function(item) {
+            $scope.showItem = item;
+
+
         }
 
     });
