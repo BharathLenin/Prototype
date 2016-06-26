9e840c672db9feb4be99ef7f63b70e75c30bce37
BXK8340 <Bharath_Kasinathan1@homedepot.com>
2016-06-25 20:55:36 -0400

    Adding events
    Added events to the dashboard sidebar and the tile components

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,9 +1,16 @@
 angular.module('myApp.controllers', [])
     .controller('homeController', function($scope, $rootScope) {
 
+    	$scope.showItem = 'Main';
+
         $scope.setSelected = function(item) {
             console.log(item);
+            console.log($scope.showItem);
         };
 
+        $scope.openComponent= function(item) {
+        	alert(item);
+        	$scope.showItem = item;
+        }
 
     });
